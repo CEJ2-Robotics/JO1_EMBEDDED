dc52c64fdf6c3c178ae7f8ad924e5e04183ad36b
fectec <fectec151@gmail.com>
2024-12-22 11:53:12 -0600

    .gitignore applied

--- CM7/Debug/Core/Src/controller/robocar_c.d
+++
@@ -1,4 +0,0 @@
-Core/Src/controller/robocar_c.o: ../Core/Src/controller/robocar_c.c \
- ../Core/Src/controller/robocar_c.h ../Core/Src/controller/rtwtypes.h
-../Core/Src/controller/robocar_c.h:
-../Core/Src/controller/rtwtypes.h:
